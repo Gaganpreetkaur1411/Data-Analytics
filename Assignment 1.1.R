d48a10bf4e1938ce96e44311a7e074c749945074
Gaganpreetkaur1411 <46419220+Gaganpreetkaur1411@users.noreply.github.com>
2019-02-02 17:07:11 +0530

    Add files via upload
    Assignment 1.1

---
+++ Assignment 1.1.R
@@ -0,0 +1,19 @@
+Assignment 1.1
+1. Say True or False for the below statements:
+  
+  . Prescriptive Analytics used to predict the future outcomes?  True
+
+. Base R packages installed automatically? True
+
+2. What is Recycling of elements in a vector?
+  
+  If we apply arithmetic operations to two vectors of unequal length, then the elements of the shorter vector are recycled to complete the operations.
+
+
+3. Give an example of recycling of elements.
+Example: x<-c(1,2,3)
+y<-c(1,8,7,6,2)
+z<-x+y
+print(z)
+
+output: [1]  2 10 10  7  4
